6894cd27078f55adbb3eda033b71f21d409322eb
Grantopher <shadowcarson@Gmail.com>
2014-04-23 12:45:53 -0700

    add guard and helpers

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+	let(:base_title) { "Petulant-Octo-Tyrion" }
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title 'Petulant-Octo-Tyrion'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+    	visit '/static_pages/home'
+    	expect(page).not_to have_title('Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
+end
